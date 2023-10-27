08ab6350f0a3a5463452badd34ac1ca498fead1c
voidwalker1707 <angeldavid3511@gmail.com>
2023-10-27 17:16:00 -0500

    Mensaje descriptivo de los cambios

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,3 @@ function App() {
 export default App;
 
 
-/* Link to 
-<Route path= "*" element = {<h2>page not found<h2/>}></Route>
-<Route path= "/:id" element = {<h2>page not found<h2/>}></Route>
-//en el parametro 
-const params  = useParams()
-params.id //
-//si se pone path= "/blog?:id el ? es para que sea opcional
-//para api  */
