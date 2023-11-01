5570cdb8b8e23cfb19ae4612294ce508d71e4d19
voidwalker1707 <angeldavid3511@gmail.com>
2023-11-01 09:01:32 -0500

    carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -4,13 +4,13 @@ function Carousel (){
             <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
             <div className="carousel-item active" data-bs-interval="10000">
-                <img src="https://cachemirasss.cl/wp-content/uploads/2018/02/slider-012-cachemiras-ss-chalecos-sueter-ropa-chile-santiago-moda-lana.jpg" className="d-block w-100" alt="..." />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_YJgo-klDV6nn6z1dBabIkt08J_bH4wjLPDn0AMly0ylo89YQaaVkqwurDY2xc5I4X7Q&usqp=CAU" className="d-block w-100" alt="..." />
             </div>
             <div className="carousel-item" data-bs-interval="2000">
-                <img src="https://cachemirasss.cl/wp-content/uploads/2018/03/slider-013-cachemiras-ss-chalecos-sueter-ropa-chile-santiago-moda-lana-1.jpg" className="d-block w-100" alt="..." />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnHcUsoR-qO3KYda3f7iTLWRZjrMWiS95j3QOKEvey03tl5uKv3wQHX8zhB7n88l8LjyQ&usqp=CAU" className="d-block w-100" alt=".." />
             </div>
             <div className="carousel-item">
-                <img src="https://cachemirasss.cl/wp-content/uploads/2018/05/slider-020-cachemiras-ss-chalecos-sueter-ropa-chile-santiago-moda-lana.jpg" className="d-block w-100" alt="..." />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZiOZnNmmvojEeT4DfKmajJrJymahJjrwckk3dLxs3XCmqXtoI2wSIC0E6kdYWNw68tl4&usqp=CAU" className="d-block w-100" alt="..." />
             </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
